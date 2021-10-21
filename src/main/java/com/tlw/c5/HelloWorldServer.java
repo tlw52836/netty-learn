f459e6f4a4c89b7299a610d31ca691ba2d6b84ab
tlw52836 <1660872260@qq.com>
2021-10-21 18:20:03 +0800

    calss

---
+++ src/main/java/com/tlw/c5/HelloWorldServer.java
@@ -0,0 +1,11 @@
+package com.tlw.c5;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+
+public class HelloWorldServer {
+    void start(){
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+    }
+}
